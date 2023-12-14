371df94688567fc7129bb44c81175a68b030277c
unknown <vinaymaheshwari35@gmail.com>
2023-12-14 18:58:44 +0530

    CHanged vercel url

--- src/Components/utils/Axios.ts
+++ src/Components/utils/Axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const CreateRequests = axios.create({
-    baseURL: 'https://events-booking-ev7s0eq97-coding-thunder.vercel.app',
+    baseURL: 'https://events-qsn4e9vks-coding-thunder.vercel.app/',
     headers: {
         "Content-Type": "application/json",
     },
